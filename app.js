ec2656f6789d73f54bd3114f5935328f8da891ac
William Watson <williamstuwatson@gmail.com>
2021-07-07 14:41:52 +0100

    fix db name

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews')
 
-const dbUrl = process.env.db_URL || 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
